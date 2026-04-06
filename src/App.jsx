9d648b26fdc1403065023a395328b1a67d93e402
Marikannan <marikannan1052007@gmail.com>
2026-04-06 21:17:06 +0530

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./home";
+import Home from "./Home";
 import MovieDetails from "./MovieDetails";
 
 function App() {
